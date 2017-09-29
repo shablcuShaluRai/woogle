949a68e8378676270639f4a8520e1bd4d28c702b
shalurai <shaluraics22@gmail.com>
2017-09-29 13:35:19 +0900

    woogle random search

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
@@ -8,7 +8,20 @@ class App extends Component {
       <div className="App">
       <h1> woogle search </h1>
       <h2> woogle random search </h2>
+
+         <div>
+           <form>
+           <input className="search-box-text" type="text" placeholder="Search for something..."/>
+           </form>
+
+
+           <a href="http://en.wikipedia.org/wiki/Special:Random"
+           class="waves-effect waves-light btn grey lighten-3 grey-text"
+           id='radomBtn'
+            target="_blank">I'm Feeling Lucky </a>
+         </div>
       </div>
+
     );
   }
 }
