9eacd85230331f1e6848719255161389e582774a
shalurai <shaluraics22@gmail.com>
2017-09-29 12:35:05 +0900

    intial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <h1> woogle search </h1>
+      <h2> woogle random search </h2>
+      </div>
+    );
+  }
+}
+
+export default App;
